4d430ed785be93a4f2d02947ca9e2b1de528e2ce
Nurmukhanbet Rakhimbayev <n.rakhimbayev@globerce.capital>
2026-02-17 01:43:55 +0500

    runserver to wsgi

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 uv run python manage.py migrate --noinput
 uv run python manage.py collectstatic --noinput --clear
-uv run python manage.py runserver 0.0.0.0:8000
+uv run gunicorn --workers 3 --bind 0.0.0.0:8000 config.wsgi:application
